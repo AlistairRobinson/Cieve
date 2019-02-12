68825e474a2b377ffab22f925a8db215d0a050ca
Nathan Hall <hallnath1@yahoo.co.uk>
2019-02-12 23:44:53 +0000

    Updated Databases connectivity

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -8,7 +8,6 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
 
     from . import auth
-    
     app.register_blueprint(auth.bp)
 
     app.config.from_mapping(SECRET_KEY='disgrace abstain umbilical freehand isotope staleness swerve matrimony babbling clock')
