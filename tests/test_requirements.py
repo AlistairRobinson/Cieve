c9345030b7f8e827b4ee05e41a58dd03f618df29
Alistair Robinson <A.Robinson.8@warwick.ac.uk>
2019-02-17 12:25:47 +0000

    Added job/application posting and retrieval tests

--- tests/test_requirements.py
+++ tests/test_requirements.py
@@ -1,5 +1,49 @@
 import pytest
-from flask import g, session
+import json
+from flask import g, session, jsonify
 
-def test_post_vacancy(client, jobs):
-    assert client.get('path/to/post/vacancy').status_code == 200
+def test_cli_login(client, auth):
+    assert client.get('/auth/cli/login').status_code == 200
+    response = auth.login_cli()
+
+    with client:
+        client.get('/')
+        assert session['user_id'] == 1
+        assert g.user['username'] == 'test'
+
+json = {}
+json['id'] = 1
+json['name'] = 'test'
+json['skills'] = 'something'
+
+@pytest.mark.parametrize(('data', 'message'), (
+    (json, b'Vacany post successful'),
+    ({}, b'Vacancy post unsuccessful'),
+))
+def test_post_vacancy(client, jobs, data, message):
+    response = jobs.post_vacancy(data)
+    assert message in response.data
+
+json = {}
+json['id'] = 1
+json['name'] = 'applicant'
+json['skills'] = 'something'
+
+@pytest.mark.parametrize(('data', 'message'), (
+    (json, b'Application post successful'),
+    ({}, b'Application post unsuccessful'),
+))
+def test_post_application(client, jobs, data, message):
+    response = jobs.apply_to_vacancy(data)
+    assert message in response.data
+
+json = {}
+json['id'] = 1
+
+def test_get_vacancies(client, jobs):
+    response = jobs.get_vacancies(json)
+    assert 'test' in response
+
+def test_get_vacancies(client, jobs):
+    response = jobs.retrieve_application(json)
+    assert 'applicant' in response
