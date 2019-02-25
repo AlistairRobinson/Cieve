e4752d84b4fec69eea189e8adba322a041c2449a
Alistair Robinson <A.Robinson.8@warwick.ac.uk>
2019-02-25 14:52:41 +0000

    Fix CSRF protection in AJAX requests

--- flaskr/static/login.js
+++ flaskr/static/login.js
@@ -5,10 +5,12 @@ $(function () {
     e.preventDefault();
     var emails = document.getElementById('emailBox').value;
     var passwords = document.getElementById('passwdBox').value;
+    var token = document.getElementById('_csrf_token').value;
 
     $.ajax({
       type: 'post',
       url: '/apl/auth/login',
+      header: {'_csrf_token': token},
       data: {email: emaisl, email: passwords},
       success: function () {
         alert('form was submitted');
--- flaskr/static/register.js
+++ flaskr/static/register.js
@@ -6,10 +6,12 @@ $(function () {
     var names = document.getElementById('regName').value;
     var emails = document.getElementById('regEmail').value;
     var passwords = document.getElementById('regPasswd').value;
+    var token = document.getElementById('_csrf_token').value;
 
     $.ajax({
       type: 'post',
       url: '/apl/auth/register',
+      header: {'_csrf_token': token},
       data: {name: names, email: emails, password: passwords},
       success: function () {
         alert('form was submitted');
